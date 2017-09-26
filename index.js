1ce69f5cbd9e593cefb9e6e240fd568783f93903
Tobias Lystad <tobias24@me.com>
2017-09-26 11:37:57 +0200

    Typos, install nodemon as devdependency

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 
 // Import libraries
 var express = require('express');
+var five = require('johnny-five');
+var socket = require('socket.io');
 
 // Server app setup
 var app = express();
-var server = app.listen(80, function {
+var server = app.listen(80, function() {
   console.log('Listening to port 80');
 });
 
 // Server static files
-app.use(express_static('public'));
+app.use(express.static('public'));
+
+
+// Create new johnny five board
+let board = new five.Board();
 
 // Set up socket.io and pass server
 var io = socket('server');
