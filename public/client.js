fd4784a52439f5b7de263a6417afe1f4ac246e36
Tobias Lystad <tobias24@me.com>
2017-10-02 22:58:08 +0200

    Create button server/client cooperation

--- public/client.js
+++ public/client.js
@@ -6,3 +6,14 @@ var socket = io.connect('http://localhost:4000');
 socket.on('ping', (data) => {
 	document.write(data.message);
 });
+
+// React to clicks: -=TODO=-
+socket.on('click', (data) => {
+	if (data.click.button === 'p1') {
+		// B1 pressed
+		console.log('Button 1 recieved!');
+	} else if (data.clock.button === 'p2') {
+		// B2 pressed
+		console.log('Button 2 recieved!');
+	}
+});
