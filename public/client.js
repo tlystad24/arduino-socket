127e1c4165fcc795515bd69aead0daafc4b3d0fe
Tobias Lystad <tobias24@me.com>
2017-10-24 18:05:56 +0200

    Refactor progress

--- public/client.js
+++ public/client.js
@@ -7,47 +7,32 @@ const server = 'localhost:4000'; // Adress in which the client can reach the ser
 const prefix = 'http://';
 const socket = io.connect(prefix + server); // -= Connect to server =-
 
-// Game object
-const game = {
-	// Text notification display
-	displayText: document.getElementById('displayText'),
-	// Player 1 symbol
-	player1: document.getElementById('player1'),
-	// Player 2 symbol
-	player2: document.getElementById('player2'),
-	// Game start date
-	startDate: Date.now(),
-	// Set winner to false
-	winner: false
-};
 
-console.log(game.display, game.player1, game.player2);
+let winner = false;
+let countDown = 5000 * (Math.random() * 2);
+let time = null; // Will be assigned later
+let clickedTime = null; // Will be assigned upon button press
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let display = document.getElementById('display');
+let init = document.getElementById('init');
+let button1 = null;
+let button2 = null;
+let suffix = 'ms';
 
-// React to clicks
+// Clicks recieved from server
 socket.on('click', (data) => {
 	game.winner = true;
 	if (data.button === 'p1') {
-		// B1 pressed
-		console.log('Button 1 recieved!');
-		game.displayText.textContent = 'Button 1';
-		game.player1.style.color = 'green';
-		game.player2.style.color = 'red';
+		// DO:
 	} else if (data.button === 'p2') {
-		// B2 pressed
-		console.log('Button 2 recieved!');
-		game.displayText.textContent = 'Button 2';
-		game.player2.style.color = 'green';
-		game.player1.style.color = 'red';
+		// DO:
 	}
 });
 
-/*
-// Input handler
-const handler = (data) => {
-	// Which button was pressed?
-	let playerRequest;
-	data.button === 'p1' ? playerRequest = 1 : playerRequest = 2;
+// Main function
 
-
-};
-*/
+let main = async () => {
+	time = Date.now();
+	
+}
